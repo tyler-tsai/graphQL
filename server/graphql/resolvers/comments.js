12dac8f446813e08868f0cb0354c7c5c7fa0e325
Tyler <Tyler.Tsai@baifu-tech.net>
2019-09-25 18:59:15 +0800

    comment and like

---
+++ server/graphql/resolvers/comments.js
@@ -0,0 +1,27 @@
+const { UserInputError } = require("apollo-server");
+const Post = require("../../models/Post");
+
+module.exports = {
+  Mutation: {
+    createComment: async (_, { postId, body }, context) => {
+      const user = checkAuth(context);
+      if (body.trim() === "") {
+        throw new UserInputError("Empty comment", {
+          errors: {
+            body: "Comment body must not empty"
+          }
+        });
+      }
+      const post = await Post.findById(postId);
+      if (post) {
+        postId.comments.unshift({
+          body,
+          username,
+          createdAt: new Date().toISOString()
+        });
+        await post.save();
+        return postId;
+      } else throw new UserInputError("Post not found");
+    }
+  }
+};
